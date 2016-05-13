19316ab4aeda9d5212da0d494f28f749a8c4ccea
EveryWell <aneverywell@gmail.com>
2016-05-13 11:52:48 +0200

    not even bad

---
+++ dist/browserify/include-all.js
@@ -0,0 +1,14 @@
+require('../directives/select');
+require('../directives/datepicker');
+require('../directives/geocomplete');
+require('../directives/numeric');
+require('../directives/bootstraptable');
+require('../directives/tooltip');
+require('../directives/validate');
+require('../directives/autocomplete');
+require('../directives/dropzone');
+require('../directives/calendar');
+require('../directives/editor');
+require('../directives/selectize');
+require('../directives/timepicker');
+require('../directives/slider');
