639aaa5f7db6e779ed8bdee5f2f05f2ce11bf6a5
Daniele Grassi <grassi@dmdigital.it>
2016-08-10 11:01:56 +0200

    Added dirty-form-message to v-dirty-form

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # vue-essentials
 Some VueJS essentials directives.
+
+## v-dirty-form
+
+*Params*
+- dirty-form-message: message to be displayed when leaving page with unsaved data.
--- src/directives/dirty-form.js
+++ src/directives/dirty-form.js
@@ -2,10 +2,18 @@
 Vue.directive('dirty-form', {
     priority: 1000,
 
+    params: ['dirty-form-message'],
+
     bind: function () {
 
+        if (typeof this.params.dirtyFormMessage == 'undefined') {
+            this.params.dirtyFormMessage = 'Ci sono modifiche non salvate, sei sicuro di lasciare la pagina?';
+        }
+
+        var _self = this;
+
         $(this.el).areYouSure({
-            'message': 'Non hai salvato le tue modifiche. Sei sicuro di voler lasciare la pagina?'
+            'message': _self.params.dirtyFormMessage
         });
 
     },
