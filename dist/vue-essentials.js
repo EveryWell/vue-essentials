52a0cc74a6289b404336482b89158d396d41f443
EveryWell <aneverywell@gmail.com>
2016-12-09 17:17:16 +0100

    added 'format' parameter to datepicker directive

--- dist/vue-essentials.js
+++ dist/vue-essentials.js
@@ -212,16 +212,22 @@ Vue.directive('calendar', {
 Vue.directive('datepicker', {
     priority: 1000,
 
-    params: ['time-picker', 'step'],
+    params: ['time-picker', 'step', 'format'],
 
     bind: function () {
+
+        var format = this.params.timePicker ? 'd/m/Y H:i' : 'd/m/Y';
+
+        if (this.params.format) {
+            format = this.params.format;
+        }
 
         $(this.el)
             .datetimepicker({
                 lang: 'it',
                 timepicker: this.params.timePicker ? this.params.timePicker : false,
                 step: this.params.step ? this.params.step : 60,
-                format: this.params.timePicker ? 'd/m/Y H:i' : 'd/m/Y',
+                format: format,
                 scrollInput: false/*,
                  defaultDate: new Date(),
                  defaultTime:'05:00'*/
