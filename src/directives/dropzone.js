94747ae6d8c358ccb1fdda0c86c4ada8353276ac
EveryWell <aneverywell@gmail.com>
2017-01-24 15:34:04 +0100

    added xcsrf token to dropzone directive

--- src/directives/dropzone.js
+++ src/directives/dropzone.js
@@ -10,14 +10,22 @@ Vue.directive('dropzone', {
 
         var _self = this;
 
-        this.dropzone = new Dropzone(this.el, {
+        var settings = {
             url: this.params.url,
             init: function() {
                 if (_self.params.addedfile) {
                     this.on("success", _self.params.addedfile);
                 }
             }
-        });
+        };
+
+        if (typeof Laravel !== 'undefined' && typeof Laravel.csrfToken !== 'undefined') {
+            settings.headers = {
+                'X-CSRF-TOKEN': Laravel.csrfToken
+            }
+        }
+
+        this.dropzone = new Dropzone(this.el, settings);
     },
     unbind: function () {
 
