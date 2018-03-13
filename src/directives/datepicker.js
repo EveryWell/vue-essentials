037d8f01d0ef1903e87fcb7910bf32298b51be26
martina <casari@dmdigital.it>
2018-03-13 12:25:17 +0100

    moved everything into datepicker.js

--- src/directives/datepicker.js
+++ src/directives/datepicker.js
@@ -2,10 +2,10 @@
 Vue.directive('datepicker', {
     priority: 1000,
 
-    params: ['time-picker', 'step', 'format'],
+    params: ['time-picker', 'step', 'format', 'disabled-days'],
 
     bind: function () {
-
+        var _self = this;
         var format = this.params.timePicker ? 'd/m/Y H:i' : 'd/m/Y';
 
         if (this.params.format) {
@@ -18,6 +18,9 @@ Vue.directive('datepicker', {
                 timepicker: this.params.timePicker ? this.params.timePicker : false,
                 step: this.params.step ? this.params.step : 60,
                 format: format,
+                beforeShowDay: _self.params.disabledDays
+                    ? function(date){ return [!_self.params.disabledDays.includes(date.getDay()), ""]; }
+                    : true,
                 scrollInput: false/*,
                  defaultDate: new Date(),
                  defaultTime:'05:00'*/
